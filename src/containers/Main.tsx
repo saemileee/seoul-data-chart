30ca98193908d12a90ad2ea6e99a578e439cf212
saem <saemilee01@gmail.com>
2023-09-10 23:35:29 +0900

    feat: 차트 추가

--- src/containers/Main.tsx
+++ src/containers/Main.tsx
@@ -1,5 +1,31 @@
+import useSeoulData from '../hooks/controllers/useSeoulChart';
+import {ComposedChart, Area, Bar, XAxis, YAxis, Tooltip, Legend} from 'recharts';
+
 const Main = () => {
-    return <div>메인</div>;
+    const {state: chartState} = useSeoulData();
+    const {data: chartData} = chartState;
+    console.info(chartData);
+
+    return (
+        <div>
+            <h3>차트</h3>
+            <ComposedChart width={1920} height={250} data={chartData}>
+                <XAxis dataKey='time' />
+                <YAxis yAxisId='left' />
+                <YAxis yAxisId='right' orientation='right' />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey='valueBar' barSize={20} fill='#19a1fb' yAxisId='right' />
+                <Area
+                    dataKey='valueArea'
+                    type='monotone'
+                    fill='#ff7c50'
+                    stroke='#f4a983'
+                    yAxisId='left'
+                />
+            </ComposedChart>
+        </div>
+    );
 };
 
 export default Main;
