f12a9954748878cd2e3e962520c5bf981d255035
saem <saemilee01@gmail.com>
2023-09-11 02:45:54 +0900

    feat: 커스텀훅 사용하여 차트 필터링

--- src/containers/Main.tsx
+++ src/containers/Main.tsx
@@ -1,25 +1,57 @@
+import useChartFilter from '../hooks/controllers/useChartFilter';
 import useSeoulData from '../hooks/controllers/useSeoulChart';
-import {ComposedChart, Area, Bar, XAxis, YAxis, Tooltip, Legend, TooltipProps} from 'recharts';
+import {
+    ComposedChart,
+    Area,
+    Bar,
+    XAxis,
+    YAxis,
+    Tooltip,
+    Legend,
+    TooltipProps,
+    Cell,
+} from 'recharts';
 
 const Main = () => {
     const {state: chartState} = useSeoulData();
     const {data: chartData} = chartState;
-    console.info(chartData);
+
+    const {selectedKey, filterOptions, setSelectedKey} = useChartFilter('id', chartData);
 
     return (
         <div>
             <h3>차트</h3>
+            <ul>
+                {filterOptions.map(option => (
+                    <li
+                        className={`${selectedKey === option && 'selected'}`}
+                        key={option}
+                        onClick={() => setSelectedKey(option)}
+                    >
+                        {option}
+                    </li>
+                ))}
+            </ul>
             <ComposedChart width={1920} height={250} data={chartData}>
                 <XAxis dataKey='time' />
                 <YAxis yAxisId='left' />
                 <YAxis yAxisId='right' orientation='right' />
                 <Tooltip content={<CustomTooltip />} />
                 <Legend />
-                <Bar dataKey='value_bar' barSize={20} fill='#19a1fb' yAxisId='right' />
+                <Bar dataKey='value_bar' opacity={0.8} barSize={20} fill='#9fd9ff' yAxisId='right'>
+                    {chartData.map((entry, index) => (
+                        <Cell
+                            cursor='pointer'
+                            fill={entry.id === selectedKey ? ' #1fa5ff' : ' #9fd9ff'}
+                            key={`cell-${index}`}
+                            onClick={() => setSelectedKey(entry.id)}
+                        />
+                    ))}
+                </Bar>
                 <Area
                     dataKey='value_area'
                     type='monotone'
-                    fill='#ff7c50'
+                    fill='#ffab8fb8'
                     stroke='#f4a983'
                     yAxisId='left'
                 />
