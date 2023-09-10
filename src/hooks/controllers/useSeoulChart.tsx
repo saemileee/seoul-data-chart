ea148f10b7888d5a85ce8dddc359337bbef6756b
saem <saemilee01@gmail.com>
2023-09-10 23:35:11 +0900

    feat: 차트용 데이터로 가공

--- src/hooks/controllers/useSeoulChart.tsx
+++ src/hooks/controllers/useSeoulChart.tsx
@@ -1,12 +1,12 @@
-import {useEffect, useReducer} from 'react';
+import {Reducer, useEffect, useReducer} from 'react';
 import {AxiosError} from 'axios';
 import * as Type from '../../types/seoulInfo.ts';
 import {getSeoulInfo} from '../../api/seoulInfo.ts';
 
-interface TypeSeoulInfoState {
+interface TypeSeoulChart {
     isLoading: boolean;
     error: null | Error;
-    data: Type.ResponseData;
+    data: Type.SeoulInfoChartItem[];
 }
 
 type TypeAction =
@@ -18,13 +18,22 @@ type TypeAction =
 const initState = {
     isLoading: true,
     error: null,
-    data: {},
+    data: [{id: '', time: '', valueArea: 0, valueBar: 0}],
 };
 
-const reducer = (state: TypeSeoulInfoState, action: TypeAction) => {
+const reducer: Reducer<TypeSeoulChart, TypeAction> = (
+    state: TypeSeoulInfoState,
+    action: TypeAction
+) => {
     switch (action.type) {
         case 'GET': {
-            return {...state, isLoading: false, data: action.payload};
+            const data = action.payload;
+            const newData = Object.entries(data).map(data => {
+                const [time, values] = data;
+                const {id, value_area: valueArea, value_bar: valueBar} = values;
+                return {id, time, valueArea, valueBar};
+            });
+            return {...state, isLoading: false, data: newData};
         }
         case 'ERROR':
             return {...state, isLoading: false, error: action.payload};
