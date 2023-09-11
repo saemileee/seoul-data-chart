69c1907dee450e8059ca93766a39a2639153b0bc
saem <saemilee01@gmail.com>
2023-09-11 16:09:36 +0900

    feat: refarea 선택 시 하이라이팅, 커스텀 툴팁 props 구조분해할당

--- src/containers/Main.tsx
+++ src/containers/Main.tsx
@@ -12,6 +12,7 @@ import {
     DotProps,
     TooltipProps,
     Cell,
+    ReferenceArea,
 } from 'recharts';
 
 const Main = () => {
@@ -34,26 +35,20 @@ const Main = () => {
                     </li>
                 ))}
             </ul>
-            <ComposedChart width={1920} height={250} data={chartData}>
+            <ComposedChart width={980} height={250} data={chartData} onClick={console.info}>
                 <XAxis label='2023년' dataKey='time' />
                 <YAxis yAxisId='left' />
                 <YAxis yAxisId='right' orientation='right' />
                 <Tooltip content={<CustomTooltip />} />
                 <Legend />
                 <Brush />
-                <defs>
-                    <linearGradient id='colorBar' x1='0' y1='0' x2='0' y2='1'>
-                        <stop offset='5%' stopColor='#82ca9d' stopOpacity={1} />
-                        <stop offset='95%' stopColor='#82ca9d' stopOpacity={0.4} />
-                    </linearGradient>
-                </defs>
+
                 <Bar dataKey='value_bar' fill='#82ca9d' barSize={20} yAxisId='right'>
                     {chartData.map((entry, index) => (
                         <Cell
                             cursor='pointer'
-                            fill={entry.id === selectedKey ? ' #01b07b' : 'url(#colorBar)'}
+                            fill={entry.id === selectedKey ? ' #01b07b' : '#a7e9c0'}
                             key={`cell-${index}`}
-                            onClick={() => setSelectedKey(entry.id)}
                         />
                     ))}
                 </Bar>
@@ -71,6 +66,22 @@ const Main = () => {
                     yAxisId='left'
                     dot={<CustomDot selectedKey={selectedKey} />}
                 />
+                {chartData.map(data => {
+                    const {time, id} = data;
+                    return (
+                        <ReferenceArea
+                            key={time}
+                            yAxisId='right'
+                            x1={time}
+                            x2={time}
+                            fill='#7ef5fb'
+                            opacity={`${id === selectedKey ? 0.5 : 0}`}
+                            onClick={() => {
+                                setSelectedKey(id);
+                            }}
+                        />
+                    );
+                })}
             </ComposedChart>
         </div>
     );
@@ -87,18 +98,20 @@ const CustomDot = ({cx, cy, selectedKey, payload}: CustomDotProps) => {
     const {id} = payload;
 
     if (id === selectedKey) {
-        return <circle cx={cx} cy={cy} r={4} stroke='#ff6b39' stroke-width='1' fill='white' />;
+        return <circle cx={cx} cy={cy} r={4} stroke='#ff6b39' strokeWidth='1' fill='white' />;
     }
     return <></>;
 };
 
 const CustomTooltip = ({active, payload}: TooltipProps<number, string>): JSX.Element | null => {
     if (active && payload && payload.length) {
+        const item = payload[0].payload;
+        const {id, value_area, value_bar} = item;
         return (
             <div className='custom-tooltip'>
-                <p className='id'>{`${payload[0].payload.id}`}</p>
-                <p className='valueArea'>{`value_area: ${payload[0].payload.value_area}`}</p>
-                <p className='valueBar'>{`value_bar: ${payload[0].payload.value_bar}`}</p>
+                <p className='id'>{`${id}`}</p>
+                <p className='valueArea'>{`value_area: ${value_area}`}</p>
+                <p className='valueBar'>{`value_bar: ${value_bar}`}</p>
             </div>
         );
     }
