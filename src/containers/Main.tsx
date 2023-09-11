3c0ae4a67661c0c8ca3b6c4be7c956f559ece28e
saem <saemilee01@gmail.com>
2023-09-11 13:12:18 +0900

    feat: brush 추가 및 선택한 아이디의 area dot 커스텀

--- src/containers/Main.tsx
+++ src/containers/Main.tsx
@@ -3,11 +3,13 @@ import useSeoulData from '../hooks/controllers/useSeoulChart';
 import {
     ComposedChart,
     Area,
+    Brush,
     Bar,
     XAxis,
     YAxis,
     Tooltip,
     Legend,
+    DotProps,
     TooltipProps,
     Cell,
 } from 'recharts';
@@ -33,27 +35,41 @@ const Main = () => {
                 ))}
             </ul>
             <ComposedChart width={1920} height={250} data={chartData}>
-                <XAxis dataKey='time' />
+                <XAxis label='2023년' dataKey='time' />
                 <YAxis yAxisId='left' />
                 <YAxis yAxisId='right' orientation='right' />
                 <Tooltip content={<CustomTooltip />} />
                 <Legend />
-                <Bar dataKey='value_bar' opacity={0.8} barSize={20} fill='#9fd9ff' yAxisId='right'>
+                <Brush />
+                <defs>
+                    <linearGradient id='colorBar' x1='0' y1='0' x2='0' y2='1'>
+                        <stop offset='5%' stopColor='#82ca9d' stopOpacity={1} />
+                        <stop offset='95%' stopColor='#82ca9d' stopOpacity={0.4} />
+                    </linearGradient>
+                </defs>
+                <Bar dataKey='value_bar' fill='#82ca9d' barSize={20} yAxisId='right'>
                     {chartData.map((entry, index) => (
                         <Cell
                             cursor='pointer'
-                            fill={entry.id === selectedKey ? ' #1fa5ff' : ' #9fd9ff'}
+                            fill={entry.id === selectedKey ? ' #01b07b' : 'url(#colorBar)'}
                             key={`cell-${index}`}
                             onClick={() => setSelectedKey(entry.id)}
                         />
                     ))}
                 </Bar>
+                <defs>
+                    <linearGradient id='colorArea' x1='0' y1='0' x2='0' y2='1'>
+                        <stop offset='5%' stopColor='#ff6b39' stopOpacity={1} />
+                        <stop offset='95%' stopColor='#ff6b39' stopOpacity={0.3} />
+                    </linearGradient>
+                </defs>
                 <Area
                     dataKey='value_area'
                     type='monotone'
-                    fill='#ffab8fb8'
-                    stroke='#f4a983'
+                    fill='url(#colorArea)'
+                    stroke='#ff6b39'
                     yAxisId='left'
+                    dot={<CustomDot selectedKey={selectedKey} />}
                 />
             </ComposedChart>
         </div>
@@ -61,6 +77,20 @@ const Main = () => {
 };
 
 export default Main;
+
+interface CustomDotProps extends DotProps {
+    selectedKey: string | number | null;
+    payload?: any;
+}
+
+const CustomDot = ({cx, cy, selectedKey, payload}: CustomDotProps) => {
+    const {id} = payload;
+
+    if (id === selectedKey) {
+        return <circle cx={cx} cy={cy} r={4} stroke='#ff6b39' stroke-width='1' fill='white' />;
+    }
+    return <></>;
+};
 
 const CustomTooltip = ({active, payload}: TooltipProps<number, string>): JSX.Element | null => {
     if (active && payload && payload.length) {
