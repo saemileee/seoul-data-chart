4421b1c204d5e81e8598447b068ffc3f5149a8fd
saem <saemilee01@gmail.com>
2023-09-11 23:41:54 +0900

    refactor: 데이터가 있는 경우에만 차트 그리기

--- src/containers/Main.tsx
+++ src/containers/Main.tsx
@@ -4,18 +4,27 @@ import ChartFilter from '../components/Chart/ChartFilter';
 import useChart from '../hooks/useChart';
 import useChartFilter from '../hooks/useChartFilter';
 const Main = () => {
-    const {data: chartData} = useChart(getSeoulInfo);
+    const {data: chartData, isLoading, error} = useChart(getSeoulInfo);
     const {selectedKey, filterOptions, setSelectedKey} = useChartFilter('id', chartData);
 
+    if (error) <div>차트를 불러오지 못했습니다.</div>;
     return (
         <div>
             <h3>차트</h3>
-            <ChartFilter
-                selectedKey={selectedKey}
-                setSelectedKey={setSelectedKey}
-                filterOptions={filterOptions}
-            />
-            <Chart data={chartData} selectedKey={selectedKey} setSelectedKey={setSelectedKey} />
+            {!isLoading && chartData.length > 0 && (
+                <>
+                    <ChartFilter
+                        selectedKey={selectedKey}
+                        setSelectedKey={setSelectedKey}
+                        filterOptions={filterOptions}
+                    />
+                    <Chart
+                        data={chartData}
+                        selectedKey={selectedKey}
+                        setSelectedKey={setSelectedKey}
+                    />
+                </>
+            )}
         </div>
     );
 };
