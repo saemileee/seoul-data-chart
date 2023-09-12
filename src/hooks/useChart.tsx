df43e3e292607953433eaaf28316bc51922ded2a
saem <saemilee01@gmail.com>
2023-09-13 07:34:20 +0900

    feat: 시계열 차트 데이터의 시간이 겹치는 부분 포맷팅하기

--- src/hooks/useChart.tsx
+++ src/hooks/useChart.tsx
@@ -1,23 +1,32 @@
 import {useEffect, useState} from 'react';
 import useFetch from './useFetch';
-import {ChartItem, ResponseData} from '../types/chartInfo';
+import {ChartData, ResponseData} from '../types/chartInfo';
+import extractCommonTime from '../utils/extractCommonTime';
+import formatDate from '../utils/formatDate';
 
 const useChart = (cb: () => Promise<ResponseData>) => {
     const {state: fetchState} = useFetch(cb);
     const {data: fetchData, isLoading, error} = fetchState;
-    const [data, setData] = useState<ChartItem[]>([]);
+    const [data, setData] = useState<ChartData>({
+        commonTime: '',
+        data: [],
+    });
 
     useEffect(() => {
         if (fetchData) {
+            const timeData = Object.entries(fetchData).map(data => data[0]);
+            const prefix = extractCommonTime(timeData);
+            const commonTime = formatDate(prefix);
+
             const newData = Object.entries(fetchData).map(data => {
                 const [time, values] = data;
-                return {time: time.replace('2023-', ''), ...values};
+                return {time: time.replace(prefix, ''), ...values};
             });
-            setData(newData);
+            setData({commonTime, data: newData});
         }
     }, [fetchData]);
 
-    return {data, isLoading, error};
+    return {chartData: data, isLoading, error};
 };
 
 export default useChart;
