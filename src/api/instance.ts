971be650eb9f6eb4b3fcfbe28e5d5bd1dec7175e
saem <saemilee01@gmail.com>
2023-09-13 08:18:13 +0900

    feat: 배포 시 cors 에러 해결을 위한  헤더 추가

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -7,6 +7,13 @@ class AxiosInstance {
     instance(endPoint: string) {
         return axios.create({
             baseURL: this.baseUrl + endPoint,
+            headers: {
+                'Access-Control-Allow-Credentials': 'true',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+                'Access-Control-Allow-Headers':
+                    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+            },
         });
     }
 }
