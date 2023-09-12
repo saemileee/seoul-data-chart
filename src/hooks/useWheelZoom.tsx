480c23f960c17831ab586b02a8ca92666076b5b2
saem <saemilee01@gmail.com>
2023-09-12 21:44:14 +0900

    fix: counts가 0일 경우 step이 infinity || nan이 되어 brush 이탈 현상 해결

--- src/hooks/useWheelZoom.tsx
+++ src/hooks/useWheelZoom.tsx
@@ -1,60 +1,76 @@
-import {useCallback, useState} from 'react';
+import {useState} from 'react';
+
+const DEFAULT_STEP = 2;
 
 const useWheelZoom = (initIdx: number[]) => {
     const [INIT_START_IDX, INIT_END_IDX] = initIdx;
     const [zoomedIdx, setZoomedIdx] = useState<number[] | null>(null);
 
-    const zoomIn = useCallback((fixedIdx: number, startIdx: number, endIdx: number) => {
+    const getSteps = (fixedIdx: number, startIdx: number, endIdx: number) => {
         const leftCounts = fixedIdx - startIdx;
         const rightCounts = endIdx - fixedIdx;
-        const [leftStep, rightStep] =
-            leftCounts < rightCounts
-                ? [1, Math.floor(rightCounts / leftCounts)]
-                : [Math.floor(leftCounts / rightCounts), 1];
 
-        if (startIdx + 1 !== endIdx && startIdx !== endIdx) {
-            setZoomedIdx([startIdx + leftStep, endIdx - rightStep]);
+        if (leftCounts !== 0 && rightCounts !== 0) {
+            const [leftStep, rightStep] =
+                leftCounts < rightCounts
+                    ? [1, Math.floor(rightCounts / leftCounts)]
+                    : [Math.floor(leftCounts / rightCounts), 1];
+            return [leftStep, rightStep, leftCounts, rightCounts];
         }
-    }, []);
-
-    const zoomOut = useCallback((fixedIdx: number, startIdx: number, endIdx: number) => {
-        const leftCounts = fixedIdx - startIdx;
-        const rightCounts = endIdx - fixedIdx;
-        const [leftStep, rightStep] =
-            leftCounts < rightCounts
-                ? [1, Math.floor(rightCounts / leftCounts)]
-                : [Math.floor(leftCounts / rightCounts), 1];
-
-        setZoomedIdx(() => {
-            const newStartIdx = Math.max(
-                startIdx - (leftCounts === rightCounts ? 2 : leftStep),
-                INIT_START_IDX
-            );
-
-            const newEndIdx = Math.min(
-                endIdx + (leftCounts === rightCounts ? 2 : rightStep),
-                INIT_END_IDX
-            );
-
-            return [newStartIdx, newEndIdx];
-        });
-    }, []);
-
-    const zoomInOrOut = useCallback(
-        (
-            e: React.WheelEvent<SVGPathElement>,
-            fixedIdx: number,
-            startIdx: number,
-            endIdx: number
-        ) => {
-            if (e.deltaY < 0) {
-                zoomIn(fixedIdx, startIdx, endIdx);
-            } else if (e.deltaY > 0) {
-                zoomOut(fixedIdx, startIdx, endIdx);
+        if (leftCounts === 0) {
+            if (rightCounts < DEFAULT_STEP) {
+                return [0, 1];
+            } else {
+                return [0, Math.floor(rightCounts / DEFAULT_STEP), leftCounts, rightCounts];
             }
-        },
-        []
-    );
+        }
+
+        if (rightCounts === 0) {
+            if (leftCounts < 2) {
+                return [1, 0];
+            } else {
+                return [Math.floor(leftCounts / DEFAULT_STEP), 0, leftCounts, rightCounts];
+            }
+        }
+        return [0, 0];
+    };
+
+    const zoomIn = (fixedIdx: number, startIdx: number, endIdx: number) => {
+        if (startIdx !== endIdx) {
+            const [leftStep, rightStep] = getSteps(fixedIdx, startIdx, endIdx);
+            if (startIdx + leftStep <= endIdx - rightStep) {
+                setZoomedIdx([startIdx + leftStep, endIdx - rightStep]);
+            }
+        }
+    };
+
+    const zoomOut = (fixedIdx: number, startIdx: number, endIdx: number) => {
+        if (
+            zoomedIdx &&
+            zoomedIdx.length > 0 &&
+            (zoomedIdx[0] !== INIT_START_IDX || zoomedIdx[1] !== INIT_END_IDX)
+        ) {
+            const [leftStep, rightStep] = getSteps(fixedIdx, startIdx, endIdx);
+
+            setZoomedIdx(prev => [
+                prev![0] - leftStep < INIT_START_IDX ? INIT_START_IDX : prev![0] - leftStep,
+                prev![1] + rightStep > INIT_END_IDX ? INIT_END_IDX : prev![1] + rightStep,
+            ]);
+        }
+    };
+
+    const zoomInOrOut = (
+        e: React.WheelEvent<SVGPathElement>,
+        fixedIdx: number,
+        startIdx: number,
+        endIdx: number
+    ) => {
+        if (e.deltaY < 0) {
+            zoomIn(fixedIdx, startIdx, endIdx);
+        } else if (e.deltaY > 0) {
+            zoomOut(fixedIdx, startIdx, endIdx);
+        }
+    };
 
     return {zoomedIdx, zoomInOrOut};
 };
