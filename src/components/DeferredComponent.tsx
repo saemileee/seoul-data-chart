696533a618e064e6fd74793e1f86ed441f7408cf
saem <saemilee01@gmail.com>
2023-09-12 23:33:52 +0900

    feat: 의도적인 로딩 지연을 위한 컴포넌트 생성

---
+++ src/components/DeferredComponent.tsx
@@ -0,0 +1,24 @@
+import {ReactNode, useEffect, useState} from 'react';
+import useDebounce from '../hooks/useDebounce';
+
+interface DeferredComponentProps {
+    loadingComponent: ReactNode;
+    children: ReactNode;
+}
+
+const DeferredComponent = ({children, loadingComponent}: DeferredComponentProps) => {
+    const [isDeferred, setIsDeferred] = useState(false);
+    const debounce = useDebounce();
+
+    useEffect(() => {
+        debounce(() => setIsDeferred(true), 700);
+    }, []);
+
+    if (!isDeferred) {
+        return <>{loadingComponent}</>;
+    }
+
+    return <>{children}</>;
+};
+
+export default DeferredComponent;
