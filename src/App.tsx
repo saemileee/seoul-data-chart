2a377a4c6480101f88d0dc2deff509cdfc1c0d05
saem <saemilee01@gmail.com>
2023-09-12 21:48:10 +0900

    refactor: 테마 토글 컴포넌트 앱으로 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import Main from './containers/Main';
 import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
-import {ThemeProvider} from './hooks/useTheme';
+import useTheme, {ThemeProvider} from './hooks/useTheme';
 import GlobalStyle from './styles/GlobalStyle';
+import ThemeToggle from './components/ThemeToggle';
 
 const App = () => {
+    const {toggleTheme, themeMode} = useTheme();
+
     return (
         <BrowserRouter>
             <ThemeProvider>
                 <GlobalStyle />
+                <ThemeToggle toggle={toggleTheme} mode={themeMode} />
                 <Routes>
                     <Route path='/*' element={<Navigate to='/chart' replace={true} />} />
                     <Route path='/chart' element={<Main />} />
