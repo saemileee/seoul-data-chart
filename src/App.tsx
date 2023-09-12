682ef2328b0c37f8af57957af8e139a7e917cf42
saem <saemilee01@gmail.com>
2023-09-12 23:34:49 +0900

    fix: 테마 토글 컴포넌트 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import Main from './containers/Main';
 import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
-import useTheme, {ThemeProvider} from './hooks/useTheme';
+import {ThemeProvider} from './hooks/useTheme';
 import GlobalStyle from './styles/GlobalStyle';
-import ThemeToggle from './components/ThemeToggle';
 
 const App = () => {
-    const {toggleTheme, themeMode} = useTheme();
-
     return (
         <BrowserRouter>
             <ThemeProvider>
                 <GlobalStyle />
-                <ThemeToggle toggle={toggleTheme} mode={themeMode} />
                 <Routes>
                     <Route path='/*' element={<Navigate to='/chart' replace={true} />} />
                     <Route path='/chart' element={<Main />} />
--- src/containers/Main.tsx
+++ src/containers/Main.tsx
@@ -2,19 +2,19 @@ import {getSeoulInfo} from '../api/seoulInfo';
 import Chart from '../components/Chart/Chart';
 import useChart from '../hooks/useChart';
 import styled from 'styled-components';
+import useTheme from '../hooks/useTheme';
+import ThemeToggle from '../components/ThemeToggle';
 
 const Main = () => {
+    const {toggleTheme, themeMode} = useTheme();
     const {data: chartData, isLoading, error} = useChart(getSeoulInfo);
 
     if (error) <div>차트를 불러오지 못했습니다.</div>;
     return (
         <StyledContainer>
             <h3>시계열 차트</h3>
-            {!isLoading && chartData.length > 0 && (
-                <>
-                    <Chart data={chartData} />
-                </>
-            )}
+            {!isLoading && chartData.length > 0 && <Chart data={chartData} />}
+            <ThemeToggle toggle={toggleTheme} mode={themeMode} />
         </StyledContainer>
     );
 };
