ff77c5154351d462dc60823e8cbf8da3b43b7969
saem <saemilee01@gmail.com>
2023-09-13 08:45:26 +0900

    refactor: BASE_URL 상수로 설정

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import {BASE_URL} from '../constants/api';
 class AxiosInstance {
     baseUrl: string;
     constructor(baseUrl: string) {
@@ -7,17 +8,10 @@ class AxiosInstance {
     instance(endPoint: string) {
         return axios.create({
             baseURL: this.baseUrl + endPoint,
-            headers: {
-                'Access-Control-Allow-Credentials': 'true',
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-                'Access-Control-Allow-Headers':
-                    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-            },
         });
     }
 }
 
-const httpClient = new AxiosInstance(import.meta.env.REACT_APP_BASE_URL || '');
+const httpClient = new AxiosInstance(BASE_URL || '');
 
 export const seoulAPI = httpClient.instance('/response');
