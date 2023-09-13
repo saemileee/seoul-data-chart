fe20f16302aebfd04dfce3d18d3ae3353bcd7bd3
saem <saemilee01@gmail.com>
2023-09-13 16:39:03 +0900

    fix: isDeferred가 false 인 경우에도 children 컴포넌트 렌더링 되게 하기

--- src/components/DeferredComponent.tsx
+++ src/components/DeferredComponent.tsx
@@ -1,5 +1,6 @@
 import {ReactNode, useEffect, useState} from 'react';
 import useDebounce from '../hooks/useDebounce';
+import styled from 'styled-components';
 
 interface DeferredComponentProps {
     loadingComponent: ReactNode;
@@ -11,14 +12,25 @@ const DeferredComponent = ({children, loadingComponent}: DeferredComponentProps)
     const debounce = useDebounce();
 
     useEffect(() => {
-        debounce(() => setIsDeferred(true), 700);
+        debounce(() => setIsDeferred(true), 500);
     }, []);
 
-    if (!isDeferred) {
-        return <>{loadingComponent}</>;
-    }
-
-    return <>{children}</>;
+    return (
+        <StyledDeferContainer>
+            <div className='loading-container'>{!isDeferred && loadingComponent}</div>
+            <div style={{opacity: isDeferred ? 1 : 0}}>{children}</div>
+        </StyledDeferContainer>
+    );
 };
 
 export default DeferredComponent;
+
+const StyledDeferContainer = styled.div`
+    position: relative;
+    .loading-container {
+        position: absolute;
+        z-index: 999;
+        left: 50%;
+        transform: translateX(-50%) translateY(10%);
+    }
+`;
